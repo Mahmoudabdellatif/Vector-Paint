35dca43f4bc2e924bd809531a9021429733e6e0d
mahmoud_abdel_latif <mahmoudabdellatif830@gmail.com>
2016-10-19 18:09:31 +0200

    last commit

---
+++ draw/src/eg/edu/alexu/csd/oop/application/Main.java
@@ -0,0 +1,16 @@
+package eg.edu.alexu.csd.oop.application;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Application g = new Application();
+		g.setVisible(true);
+		g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		g.setSize(2000, 725);
+		g.setResizable(false);
+	}
+
+}
